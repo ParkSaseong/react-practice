6e9b8d7db718cd56a52446cec38acdee83d9fb22
ParkSaseong <78298561+ParkSaseong@users.noreply.github.com>
2024-07-09 15:00:05 +0900

    practice

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
-import Wrapper from "./wrapper";
-import Text from "./text";
 import './App.css';
-
-interface TextItem {
-    text: string;
-    color: string;
-}
+import ListPage from "./page/ListPage"
 
 function App() {
-    const arr: TextItem[] = [
-        { text: "한 글자", color: "black" },
-        { text: "두 글자", color: "red" },
-        { text: "세 글자", color: "blue" }
-    ];
 
     return (
         <div className="App">
-            <Wrapper>
-                <Text text={arr[0].text} color={arr[0].color} />
-                <Text text={arr[1].text} color={arr[1].color} />
-            </Wrapper>
-            <Text text={arr[2].text} color={arr[2].color} />
+            <ListPage/>
         </div>
     );
 }
