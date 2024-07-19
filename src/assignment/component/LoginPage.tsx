1a4776fcc044d1497de56c48ff79448727b242c0
ParkSaseong <78298561+ParkSaseong@users.noreply.github.com>
2024-07-19 15:58:01 +0900

    go

--- src/assignment/component/LoginPage.tsx
+++ src/assignment/component/LoginPage.tsx
@@ -1,7 +1,10 @@
 import React, {useState} from "react";
-import styled from "styled-components";
 import styles from "../styles/loginpage.module.scss"
-import { ToastContainer, toast } from "react-toastify";
+import Wrapper from "../component/Wrapper"
+import Text from "../../Text";
+import TextInput from "./TextInput";
+import LoginCheckBox from "./LoginCheckBox";
+import {LoginPageButton, ExtraButton} from "./LoginPageButton";
 
 function LoginPage() {
     const [inputs, setInputs] = useState({
@@ -27,91 +30,70 @@ function LoginPage() {
         })
     }
 
+    const extraOnClick = () => {
+
+    }
+
     return (
         <div className={styles.loginpage}>
             <div>
                 <h1>DeamHome</h1>
             </div>
+
             {/*wrapper*/}
             <div>
                 <Wrapper>
                     <div className={styles.inwrapper}>
                         <div>
-                            <label aria-label="아이디">
-                                <input id="id" placeholder="아이디" className={styles.input} onChange={onChange} value={id}/>
-                            </label>
-                            <label aria-label="비밀번호">
-                                <input id="password" placeholder="비밀번호" className={styles.input} onChange={onChange} value={password}/>
-                            </label>
+                            <div>
+                                <TextInput  id={"id"} placeholder={"아이디"} value={id} onChange={onChange}/>
+                                <TextInput className={styles.input} id={"password"} placeholder={"비밀번호"} value={password} onChange={onChange}/>
+                                {/*<label aria-label="아이디">*/}
+                                {/*    <input id="id" placeholder="아이디" className={styles.input} onChange={onChange} value={id}/>*/}
+                                {/*</label>*/}
+                                {/*<label aria-label="비밀번호">*/}
+                                {/*    <input id="password" placeholder="비밀번호" className={styles.input} onChange={onChange} value={password}/>*/}
+                                {/*</label>*/}
+                            </div>
+
+                            <div>
+                                <LoginCheckBox id={"간편로그인"} text={"간편 로그인 정보 저장"}/>
+                                {/*<label aria-label="간편로그인" className={styles.checkbox}>*/}
+                                {/*    <input id="간편로그인" type="checkbox"/>간편 로그인 저장*/}
+                                {/*</label>*/}
+                            </div>
                         </div>
 
                         <div>
-                            <label aria-label="간편로그인" className={styles.checkbox}>
-                                <input id="간편로그인" type="checkbox"/>간편 로그인 저장
-                            </label>
+                            <LoginPageButton id={"로그인"} value={"로그인"} _onClick={LoginOnClick}/>
+                            {/*<label aria-label="로그인">*
+                            {/*    <input className={styles.loginbutton} id="로그인" type="button" value="로그인"*/}
+                            {/*           onClick={LoginOnClick}/>*/}
+                            {/*</label>*/}
                         </div>
 
-                        <div>
-                            <label aria-label="로그인">
-                                <input className={styles.loginbutton} id="로그인" type="button" value="로그인"
-                                       onClick={LoginOnClick}/>
-                            </label>
+                        <div className={styles.container}>
+                            <div className={styles.left}>
+                                <ExtraButton id={"회원가입"} value={"회원가입"} _onClick={extraOnClick}/>
+                            </div>
+                            <div className={styles.right}>
+                                <ExtraButton id={"계정찾기"} value={"계정찾기"} _onClick={extraOnClick}/>
+                                <ExtraButton id={"비밀번호찾기"} value={"비밀번호 찾기"} _onClick={extraOnClick}/>
+                            </div>
+                            {/*<label aria-label="회원가입" className={styles.extrabutton}>*/}
+                            {/*    <input id="회원가입" type="button" value="회원가입"/>*/}
+                            {/*</label>*/}
+                            {/*<label aria-label="계정찾기" className={styles.extrabutton}>*/}
+                            {/*    <input id="계정찾기" type="button" value="계정찾기" />*/}
+                            {/*</label>*/}
+                            {/*<label aria-label="비밀번호찾기" className={styles.extrabutton}>*/}
+                            {/*    <input id="비밀번호찾기" type="button" value="비밀번호 찾기"/>*/}
+                            {/*</label>*/}
                         </div>
-
-                        <div className={styles.extraButtons}>
-                            <label aria-label="회원가입" className={styles.extrabutton}>
-                                <input id="회원가입" type="button" value="회원가입"/>
-                            </label>
-                            <label aria-label="계정찾기" className={styles.extrabutton}>
-                                <input id="계정찾기" type="button" value="계정찾기" />
-                            </label>
-                            <label aria-label="비밀번호찾기" className={styles.extrabutton}>
-                                <input id="비밀번호찾기" type="button" value="비밀번호 찾기"/>
-                            </label>
-                        </div>
-
-                        {/*<div className={styles.extraButtons}>*/}
-                        {/*    <label aria-label="회원가입" className={styles.extrabutton}>*/}
-                        {/*        <input id="회원가입" type="button" value="회원가입"/>*/}
-                        {/*    </label>*/}
-                        {/*    <label aria-label="계정찾기" className={styles.extrabutton}>*/}
-                        {/*        <input id="계정찾기" type="button" value="계정찾기"/>*/}
-                        {/*    </label>*/}
-                        {/*    <label aria-label="비밀번호찾기" className={styles.extrabutton}>*/}
-                        {/*        <input id="비밀번호찾기" type="button" value="비밀번호 찾기"/>*/}
-                        {/*    </label>*/}
-                        {/*</div>*/}
                     </div>
                 </Wrapper>
             </div>
         </div>
-    );
-}
-
-interface WrapperProps {
-    children: React.ReactNode
-}
-
-interface StyleDivProps {
-    borderColor: string
-}
-
-const StyleDiv = styled.div<StyleDivProps>`
-    border: 2px solid ${props => props.borderColor};
-    padding: 16px;
-    margin: 20px auto;
-    width: 600px;
-    height: 400px;
-`
-
-const Wrapper = ({children}: WrapperProps) => {
-    return (
-        <StyleDiv borderColor="black">
-            {children}
-        </StyleDiv>
-        // <div>
-        //
-        // </div>
     );
 }
 
